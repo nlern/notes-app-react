914ca4c3529b2ad91549f65b3672eb7651b1b3da
Shantanu Dutta <shantanudutta1994@gmail.com>
2021-05-14 10:59:35 +0530

    Added 'Add New Note' button on dashboard page.

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -21,7 +21,15 @@ export default function Dashboard() {
   return (
     <section>
       <section>
-        <h1 className='text-xl my-2'>My Notes</h1>
+        <h1 className='text-xl py-4 my-2 relative'>
+          <span>My Notes</span>
+          <button
+            className='absolute right-0 px-4 py-1 text-base text-white bg-green-600 font-semibold rounded border border-green-200 hover:text-white hover:bg-green-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2'
+            title='Add New Note'
+          >
+            + New
+          </button>
+        </h1>
         <List items={notes} />
       </section>
     </section>
